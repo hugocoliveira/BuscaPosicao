0761bf0688022aadb44c2ed351fcc35388b0c008
Hugo <chagas.hugo@gmail.com>
2026-08-05 18:26:52 -0300

    chore: bump versao para 1.5 (versionCode 6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId  = "br.com.lit.busca.posicao"
         minSdk         = 24
         targetSdk      = 36
-        versionCode    = 5
-        versionName    = "1.4"
+        versionCode    = 6
+        versionName    = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
