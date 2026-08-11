800c2233f91e231d5982c6a0c63f7d7294a1e5e7
Hugo <chagas.hugo@gmail.com>
2026-08-11 14:05:31 -0300

    fix: titulo alterado para Posicao — v2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId  = "br.com.lit.busca.posicao"
         minSdk         = 24
         targetSdk      = 36
-        versionCode    = 15
-        versionName    = "2.4"
+        versionCode    = 16
+        versionName    = "2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
